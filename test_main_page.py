473702a1cd330a709791c760da3082116df777b3
pasichnyky <92795180+pasichnyky@users.noreply.github.com>
2022-11-27 11:23:16 +0100

    Первый тест на основе Page Object

---
+++ test_main_page.py
@@ -0,0 +1,8 @@
+from pages.main_page import MainPage
+
+
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()
