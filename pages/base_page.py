4be5fdd1628dca46fc004773b86c487bd11c0795
pasichnyky <92795180+pasichnyky@users.noreply.github.com>
2022-11-27 11:31:24 +0100

    Методы-проверки в Page Object. Проверка элемента на странице

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,3 +1,6 @@
+from selenium.common.exceptions import NoSuchElementException
+
+
 class BasePage:
     def __init__(self, browser, url, timeout=10):
         self.browser = browser
@@ -6,3 +9,10 @@ class BasePage:
 
     def open(self):
         self.browser.get(self.url)
+
+    def is_element_present(self, how, what):
+        try:
+            self.browser.find_element(how, what)
+        except NoSuchElementException:
+            return False
+        return True
